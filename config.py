bad7d477f8b721e7bb139aa662a00e8c219296a1
𝗞ʀɪꜱʜɴᴀ <pk4108644@gmail.com>
2026-08-11 14:47:54 +0530

    Add currency name and starting balance configurations
    Added new configurations for currency name and starting balance.

--- config.py
+++ config.py
@@ -11,6 +11,10 @@ PORT = int(os.getenv("PORT", 5000))
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "kustrobot")
 
+# Added new configurations
+CURRENCY_NAME = os.getenv("CURRENCY_NAME", "Coins")
+STARTING_BALANCE = int(os.getenv("STARTING_BALANCE", "0"))
+
 BOT_NAME = "Kust Robot"
 UPDATES_CHANNEL = "https://t.me/kustbots"
 SUPPORT_GROUP = "https://t.me/kustbotschat"
